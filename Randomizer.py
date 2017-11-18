12ee98d96e5e9b42e8e746e7c2b8397c0c15942f
Luckybugs <djurovic.jelena@gmail.com>
2017-11-18 11:33:56 +0100

    linted

--- Randomizer.py
+++ Randomizer.py
@@ -21,7 +21,7 @@ def create_ui(p_window_title, p_apply_callback, p_restore_callback):
         creating UI.
     """
     # each window has a unique identifier so only one would be opened at a time
-    window_id = 'Mywindow_id' 
+    window_id = 'Mywindow_id'
     if cmds.window(window_id, exists=True):
         cmds.deleteUI(window_id)
 
@@ -37,7 +37,7 @@ def create_ui(p_window_title, p_apply_callback, p_restore_callback):
                                        (5, 'right', 3)])
     # Move controls
     move_input_field = cmds.floatSliderGrp(label='move:', field=True,
-                                           fieldMinValue=0, fieldMaxValue=5, 
+                                           fieldMinValue=0, fieldMaxValue=5,
                                            minValue=0, maxValue=10, precision=2,
                                            value=0.01, step=.01,
                                            columnWidth3=[40, 50, 150],
@@ -60,16 +60,16 @@ def create_ui(p_window_title, p_apply_callback, p_restore_callback):
     rcbz = cmds.checkBox(label='Z', value=True)
     cmds.separator(h=20, style='none')
     # Scale controls
-    scale_input_field = cmds.floatSliderGrp(label='scale:', field=True, 
-                                            fieldMinValue=0, fieldMaxValue=1, 
-                                            minValue=0, maxValue=1, precision=2, 
+    scale_input_field = cmds.floatSliderGrp(label='scale:', field=True,
+                                            fieldMinValue=0, fieldMaxValue=1,
+                                            minValue=0, maxValue=1, precision=2,
                                             value=0.01, step=.01,
                                             columnWidth3=[40, 50, 150],
                                             columnAlign3=['left', 'both', 'left'])
     scbx = cmds.checkBox(label='X', value=True)
     scby = cmds.checkBox(label='Y', value=True)
     scbz = cmds.checkBox(label='Z', value=True)
-    
+
     cmds.separator(h=10, style='none')
     cmds.separator(h=10, style='none')
     cmds.separator(h=10, style='none')
@@ -80,7 +80,7 @@ def create_ui(p_window_title, p_apply_callback, p_restore_callback):
     # new dictionary to hold original positons..
     before_data = {}
 
-    # Buttons 
+    # Buttons
     cmds.button(label="Randomize", width=240,
                 command=functools.partial(p_apply_callback, move_input_field,
                                           mcbx, mcby, mcbz,
@@ -88,35 +88,35 @@ def create_ui(p_window_title, p_apply_callback, p_restore_callback):
                                           rcbx, rcby, rcbz,
                                           scale_input_field,
                                           scbx, scby, scbz,
-                                          before_data,))  
-    
+                                          before_data,))
+
     cmds.button(label="Restore", width=100,
                 command=functools.partial(p_restore_callback, before_data))
     cmds.separator(h=25, style='none')
-    
+
     cmds.showWindow()
 
 def translater(sel, percent, chek_boxes, before_data):
     """
-        Internal function that changes the transform attributes in the cahnel
-        box based on user input.
+        Internal function that changes the transform attributes in the
+        channel box based on user input.
     """
     axies = ['X', 'Y', 'Z']
     for item in sel:
         new_translate_values = []
-        
+
         for old_translate_value in before_data[item]['pos']:
             new_translate_values.append(old_translate_value + random.uniform(-percent, percent))
 
          #if the user has cheked the box, we change to the new value
         for box, new_value, xyz in izip(chek_boxes, new_translate_values, axies):
-            if box: 
+            if box:
                 cmds.setAttr(item + '.translate'+ xyz, new_value)
 
 def rotor(sel, percent, chek_boxes):
     """
-        Internal function that changes the rotate attributes in the chnel
-        box based on user input.
+        Internal function that changes the rotate attributes in the
+        channel box based on user input.
     """
     degree = (360.0*percent)/100
     axies = ['X', 'Y', 'Z']
@@ -125,19 +125,19 @@ def rotor(sel, percent, chek_boxes):
 
         for xyz in axies:
             new_rotate_values.append(random.uniform(0, degree))
-        
+
         #if the user has cheked the box change to new value
         for box, new_value, xyz in izip(chek_boxes, new_rotate_values, axies):
-            if box: 
+            if box:
                 cmds.setAttr(item + '.rotate'+ xyz, new_value)
-       
+
 def scaler(sel, percent, chek_boxes, before_data):
     """
-        Internal function that changes the scale attributes in the cahnel
-        box based on user input.
+        Internal function that changes the scale attributes in the
+        channel box on user input.
     """
     axies = ['X', 'Y', 'Z']
-    for item in sel: 
+    for item in sel:
         new_scale_values = []
 
         for old_scale_value in before_data[item]['scl']:
@@ -145,18 +145,18 @@ def scaler(sel, percent, chek_boxes, before_data):
 
         #if the user has cheked the box change to new value
         for box, new_value, xyz in izip(chek_boxes, new_scale_values, axies):
-            if box: 
+            if box:
                 cmds.setAttr(item + '.scale'+ xyz, new_value)
-            
+
 def find_poly_obj(sel):
     """
         Finds only poly objects in a selection.
     """
     poly_obj = []
-  
+
     for item in sel:
         shapes = cmds.listRelatives(item, shapes=True)
-    
+
         if cmds.nodeType(shapes[0]) == 'mesh':
             poly_obj.append(item)
 
@@ -164,7 +164,7 @@ def find_poly_obj(sel):
 
 def get_original_pos(polys, before_data):
     """
-        Gets original positions poly objects in a selection.
+        Gets original attributes of objects.
     """
     for item in polys:
         if item not in before_data:
@@ -183,7 +183,7 @@ def get_original_pos(polys, before_data):
 
 def restore_to_original_position(sel, poly_data):
     """
-        restores to orginal chanell box values.
+        restores to orginal channel box values.
     """
     for item in sel:
         if item in poly_data:
@@ -200,31 +200,33 @@ def apply_callback(p_move_input_field, pmcbx, pmcby, pmcbz, p_rotate_input_field
                    prcbx, prcby, prcbz, p_scale_input_field, pscbx, pscby, pscbz,
                    p_before_data, *pArgs):
     """
-        applyCallback.
-    """  
+        apply callback
+    """
+    # querying ui
     input_translate = cmds.floatSliderGrp(p_move_input_field, query=True, value=True)
-    
+
     input_mcbx = cmds.checkBox(pmcbx, query=True, value=True)
     input_mcby = cmds.checkBox(pmcby, query=True, value=True)
     input_mcbz = cmds.checkBox(pmcbz, query=True, value=True)
-    
+
     input_translate_checkbox = [input_mcbx, input_mcby, input_mcbz]
 
     input_rotate = cmds.floatSliderGrp(p_rotate_input_field, query=True, value=True)
-    
+
     input_rcbx = cmds.checkBox(prcbx, query=True, value=True)
     input_rcby = cmds.checkBox(prcby, query=True, value=True)
     input_rcbz = cmds.checkBox(prcbz, query=True, value=True)
-    
+
     input_rotate_checkbox = [input_rcbx, input_rcby, input_rcbz]
-   
-    input_scale = cmds.floatSliderGrp(p_scale_input_field, query=True, value=True) 
-    
+
+    input_scale = cmds.floatSliderGrp(p_scale_input_field, query=True, value=True)
+
     input_scbx = cmds.checkBox(pscbx, query=True, value=True)
     input_scby = cmds.checkBox(pscby, query=True, value=True)
     input_scbz = cmds.checkBox(pscbz, query=True, value=True)
-    
+
     input_scale_checkbox = [input_scbx, input_scby, input_scbz]
+    # end querying ui
 
     selection = cmds.ls(selection=True)
     sel = find_poly_obj(selection)
@@ -244,7 +246,7 @@ def restore_callback(p_before_data, *pArgs):
     """
         restoreCallback
     """
-    # get the selection
+    # get users selection
     selection = cmds.ls(selection=True)
 
     # filter to just poly objects
